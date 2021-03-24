262197f85d21608144868de9965a588b98860b05
Tien Nguyen <tiennt.june.20@gmail.com>
2021-03-24 16:41:05 +0700

    fetch remote data

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ const App = () => {
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
 
-  const [rowData, setRowData] = useState([
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 }
-  ]);
+  const [rowData, setRowData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+      .then(result => result.json())
+      .then(rowData => setRowData(rowData))
+  }, []);
+
+
 
   function onGridReady(params) {
     setGridApi(params.api);
@@ -24,8 +28,8 @@ const App = () => {
   return (
     <div className="ag-theme-material" style={{ height: 400, width: 600 }}>
       <AgGridReact onGridReady={onGridReady} rowData={rowData}>
-        <AgGridColumn field="make" sortable={ true } />
-        <AgGridColumn field="model"   filter={ true }/>
+        <AgGridColumn field="make" sortable={true} />
+        <AgGridColumn field="model" filter={true} />
         <AgGridColumn field="price" />
       </AgGridReact>
     </div>
