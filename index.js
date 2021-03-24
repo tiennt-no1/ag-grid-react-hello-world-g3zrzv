8566202d84e31053bd498dd2814ba85b74c935fa
Tien Nguyen <tiennt.june.20@gmail.com>
2021-03-24 16:41:51 +0700

    add useEffect

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import { AgGridColumn, AgGridReact } from "ag-grid-react";
 
